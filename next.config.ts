9da4b04c2d346f808ca60c77cffbfb83d565d2dc
fabiconcept <favourajokubi@gmail.com>
2025-11-26 14:01:08 +0100

    Setup environments

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      { 
+        protocol: "https",
+        hostname: "chirps-chat.sirv.com",
+        pathname: "/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
