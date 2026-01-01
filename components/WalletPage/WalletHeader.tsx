2f1eb4bddd37472885e114bd713724d2d06abc85
fabiconcept <favourajokubi@gmail.com>
2026-01-01 06:21:35 +0100

    Update WalletHeader Component remove border & shadow

--- components/WalletPage/WalletHeader.tsx
+++ components/WalletPage/WalletHeader.tsx
@@ -1,18 +1,13 @@
 "use client";
-import { ArrowLeft, Eye, EyeOff } from "lucide-react";
-import { useRouter } from "next/navigation";
-import { Button } from "../ui/button";
 import { useState } from "react";
-import { motion, AnimatePresence } from "framer-motion";
 import { WalletHeader } from "../Wallet/components";
 
 export default function WalletHeaderComponent() {
-    const router = useRouter();
     const [balanceVisible, setBalanceVisible] = useState(true);
     const balance = 12847.50;
 
     return (
-        <div className="sticky top-0 z-50 border-b border-input bg-background/95 backdrop-blur-sm shadow-lg shadow-foreground/5">
+        <div className="sticky top-[9.5%] z-20 bg-background/80 backdrop-blur-sm">
             <WalletHeader
                 balance={balance}
                 usdValue={(balance * 0.85).toString()}
